f57af40870f7e86144e16363736d701fcf26b1df
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-10 21:04:43 +0900

    create initial files

---
+++ src/main/java/com/github/miyohide/springboot_31/Springboot31Application.java
@@ -0,0 +1,13 @@
+package com.github.miyohide.springboot_31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot31Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Springboot31Application.class, args);
+  }
+
+}
