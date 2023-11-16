97183532cc480e3c7ca8acd0d4397502e2034d55
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-16 20:18:42 +0900

    remove unused import statement

--- src/main/java/com/github/miyohide/springboot_31/GreetingController.java
+++ src/main/java/com/github/miyohide/springboot_31/GreetingController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
