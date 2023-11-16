32f5f87bd4c2b248d01e7e724679ab584a60f1e4
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-16 20:16:46 +0900

    add view endpoint that show all keys

--- src/main/java/com/github/miyohide/springboot_31/GreetingController.java
+++ src/main/java/com/github/miyohide/springboot_31/GreetingController.java
@@ -3,17 +3,25 @@ package com.github.miyohide.springboot_31;
 import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 @RestController
 public class GreetingController {
   private final HttpSession httpSession;
+  private final RedisTemplate<String, String> redisTemplate;
   private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
-  public GreetingController(HttpSession httpSession) {
+  public GreetingController(HttpSession httpSession, RedisTemplate<String, String> redisTemplate) {
     this.httpSession = httpSession;
+    this.redisTemplate = redisTemplate;
   }
 
   @GetMapping("/")
@@ -26,6 +34,17 @@ public class GreetingController {
         httpSession.getId(),
         (String) httpSession.getAttribute("name"),
         httpSession.getCreationTime());
+  }
+
+  @GetMapping("/view")
+  public String viewSession() {
+    Set<String> redisKeys = redisTemplate.keys("*");
+    List<String> keysList = new ArrayList<>();
+    Iterator<String> it = redisKeys.iterator();
+    while (it.hasNext()) {
+      keysList.add(it.next());
+    }
+    return keysList.toString();
   }
 
   @GetMapping("/goodbye")
