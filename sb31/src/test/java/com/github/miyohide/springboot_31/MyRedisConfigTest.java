61b838ccf756dc0d471762272628926bed0a335b
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-23 14:41:00 +0900

    delete unused import statement.

--- sb31/src/test/java/com/github/miyohide/springboot_31/MyRedisConfigTest.java
+++ sb31/src/test/java/com/github/miyohide/springboot_31/MyRedisConfigTest.java
@@ -1,6 +1,5 @@
 package com.github.miyohide.springboot_31;
 
-import io.lettuce.core.ClientOptions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
