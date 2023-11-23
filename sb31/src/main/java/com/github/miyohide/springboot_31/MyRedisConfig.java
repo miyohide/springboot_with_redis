4386c7e5085b9525a2328e5a2dbd9ebf74bdccf6
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-23 20:23:12 +0900

    check customize TCP_USER_TIMEOUT

--- sb31/src/main/java/com/github/miyohide/springboot_31/MyRedisConfig.java
+++ sb31/src/main/java/com/github/miyohide/springboot_31/MyRedisConfig.java
@@ -1,5 +1,11 @@
 package com.github.miyohide.springboot_31;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.resource.NettyCustomizer;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.epoll.EpollChannelOption;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +14,21 @@ import org.springframework.context.annotation.Configuration;
 public class MyRedisConfig {
   @Bean
   public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+    ClientResources clientResources =
+        ClientResources.builder()
+            .nettyCustomizer(
+                new NettyCustomizer() {
+                  @Override
+                  public void afterBootstrapInitialized(Bootstrap bootstrap) {
+                    bootstrap.option(EpollChannelOption.TCP_USER_TIMEOUT, 123452);
+                  }
+                })
+            .build();
+    SocketOptions socketOptions = SocketOptions.builder().keepAlive(true).build();
+    ClientOptions clientOptions = ClientOptions.builder().socketOptions(socketOptions).build();
     return clientConfigurationBuilder -> {
-      if (clientConfigurationBuilder.build().isUseSsl()) {
-        clientConfigurationBuilder.useSsl().disablePeerVerification();
-      }
+      clientConfigurationBuilder.clientOptions(clientOptions);
+      clientConfigurationBuilder.clientResources(clientResources);
     };
   }
 }
--- sb31/src/test/java/com/github/miyohide/springboot_31/MyRedisConfigTest.java
+++ sb31/src/test/java/com/github/miyohide/springboot_31/MyRedisConfigTest.java
@@ -1,29 +1,44 @@
 package com.github.miyohide.springboot_31;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.resource.ClientResources;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.epoll.EpollChannelOption;
+import io.netty.util.Attribute;
+import io.netty.util.AttributeKey;
+import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.SingleThreadEventExecutor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.TestPropertySource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
-@TestPropertySource(properties = {"spring.data.redis.ssl.enabled=true"})
 public class MyRedisConfigTest {
-  @Autowired
-  private LettuceConnectionFactory lettuceConnectionFactory;
+  @Autowired private LettuceConnectionFactory lettuceConnectionFactory;
 
-  @Autowired
-  private RedisTemplate<String, String> redisTemplate;
+  @Autowired private RedisTemplate<String, String> redisTemplate;
 
   @Test
   public void testLettuceClientConfigurationCustomization() {
     assertNotNull(lettuceConnectionFactory, "LettuceConnectionFactory is null");
     assertNotNull(redisTemplate, "RedisTemplate is null");
 
-    assertTrue(lettuceConnectionFactory.isUseSsl());
-    assertFalse(lettuceConnectionFactory.isVerifyPeer());
+    ClientOptions clientOptions = lettuceConnectionFactory.getClientConfiguration().getClientOptions().get();
+    ClientResources clientResources = lettuceConnectionFactory.getClientResources();
+    assertTrue(clientOptions.getSocketOptions().isKeepAlive());
+
+    Bootstrap bootstrap = new Bootstrap();
+    clientResources.nettyCustomizer().afterBootstrapInitialized(bootstrap);
+    String tcpUserTimeout = bootstrap.config().options().get(EpollChannelOption.TCP_USER_TIMEOUT).toString();
+    assertEquals("123452", tcpUserTimeout);
   }
 }
