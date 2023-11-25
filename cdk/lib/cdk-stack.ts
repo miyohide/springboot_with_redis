12e1554c167169fc4c0c676db1d31efed0e47964
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-25 16:23:35 +0900

    update project stack

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Network } from './construct/network';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const { vpc, appRunnerSecurityGroup, cacheSecurityGroup } = new Network(this, 'Network');
   }
 }
