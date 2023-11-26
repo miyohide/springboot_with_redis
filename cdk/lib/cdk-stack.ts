2541c66025839fbd0f72c4da9a6c1b655e92eb0e
Hidenori Miyoshi <miyohide@gmail.com>
2023-11-26 15:46:30 +0900

    create ecr

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,11 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Network } from './construct/network';
+import { EcrRepository } from './construct/ecr-repository';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // VPCを作成する
     const { vpc, appRunnerSecurityGroup, cacheSecurityGroup } = new Network(this, 'Network');
+
+    // ECRを作成する
+    const { repository } = new EcrRepository(this, 'Ecr');
   }
 }
