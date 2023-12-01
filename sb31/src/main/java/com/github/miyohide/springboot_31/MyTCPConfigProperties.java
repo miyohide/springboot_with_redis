34be9c688b3a2789058e02ba73e9008faab3f987
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-01 20:34:40 +0900

    create custom property file

---
+++ sb31/src/main/java/com/github/miyohide/springboot_31/MyTCPConfigProperties.java
@@ -0,0 +1,18 @@
+package com.github.miyohide.springboot_31;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "my-tcp-config")
+public class MyTCPConfigProperties {
+  private int tcp_user_timeout;
+
+  public int getTcp_user_timeout() {
+    return tcp_user_timeout;
+  }
+
+  public void setTcp_user_timeout(int tcp_user_timeout) {
+    this.tcp_user_timeout = tcp_user_timeout;
+  }
+}
