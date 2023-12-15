ae47c5f79c4e39363f670d6934ac3356b0142afa
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-15 21:19:32 +0900

    use interface

--- cdk/lib/cache-stack.ts
+++ cdk/lib/cache-stack.ts
@@ -4,16 +4,21 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { CfnCacheCluster } from 'aws-cdk-lib/aws-elasticache';
 
+interface CacheStackProps extends StackProps {
+  vpc: Vpc;
+  cacheSecurityGroup: SecurityGroup;
+};
+
 export class CacheStack extends Stack {
   public readonly cacheCluster: CfnCacheCluster;
 
-  constructor(scope: Construct, id: string, vpc: Vpc, cacheSecurityGroup: SecurityGroup, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: CacheStackProps) {
     super(scope, id, props);
 
     // Cacheを作成する
     this.cacheCluster = new Cache(this, 'ElastiCache', {
-      vpc,
-      cacheSecurityGroup,
+      vpc: props.vpc,
+      cacheSecurityGroup: props.cacheSecurityGroup,
     }).cacheCluster;
   }
 }
