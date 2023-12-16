d8deb1cd055fcbabffd2efde3a89ca6beb3069a8
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-16 20:51:45 +0900

    use interface

--- cdk/lib/compute-stack.ts
+++ cdk/lib/compute-stack.ts
@@ -5,16 +5,23 @@ import { SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { CfnCacheCluster } from 'aws-cdk-lib/aws-elasticache';
 
+interface ComputeStackProps extends StackProps {
+  vpc: Vpc;
+  repository: Repository;
+  appRunnerSecurityGroup: SecurityGroup;
+  cacheCluster: CfnCacheCluster;
+};
+
 export class ComputeStack extends Stack {
-  constructor(scope: Construct, id: string, vpc: Vpc, repository: Repository, appRunnerSecurityGroup: SecurityGroup, cacheCluster: CfnCacheCluster,  props?: StackProps) {
+  constructor(scope: Construct, id: string, props: ComputeStackProps) {
     super(scope, id, props);
 
     // App Runnerを作成する
     new AppRunner(this, 'AppRunner', {
-      vpc,
-      repository,
-      appRunnerSecurityGroup,
-      cacheCluster,
+      vpc: props.vpc,
+      repository: props.repository,
+      appRunnerSecurityGroup: props.appRunnerSecurityGroup,
+      cacheCluster: props.cacheCluster,
     });
   }
 }
