bf44d3673e6af2f8d6e813758943632bd2d56f46
Hidenori Miyoshi <miyohide@gmail.com>
2023-12-16 20:53:10 +0900

    use network, ecr, cache and compute Stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,9 +1,31 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { NetworkStack } from '../lib/network-stack';
+import { ContainerRepositoryStack } from '../lib/container-repository-stack';
+import { CacheStack } from '../lib/cache-stack';
+import { ComputeStack } from '../lib/compute-stack';
 
 const app = new cdk.App();
 
-new CdkStack(app, 'CdkStack', {
+// new CdkStack(app, 'CdkStack', {
+// });
+
+const networkStack = new NetworkStack(app, 'NetworkStack');
+const ecrStack = new ContainerRepositoryStack(app, 'ContainerRepositoryStack');
+const cacheStack = new CacheStack(app, 'CacheStack', {
+    vpc: networkStack.vpc,
+    cacheSecurityGroup: networkStack.cacheSecurityGroup
 });
+
+const computeStack = new ComputeStack(app, 'ComputeStack', {
+    vpc: networkStack.vpc,
+    repository: ecrStack.repository,
+    appRunnerSecurityGroup: networkStack.appRunnerSecurityGroup,
+    cacheCluster: cacheStack.cacheCluster
+});
+
+cacheStack.addDependency(networkStack);
+computeStack.addDependency(networkStack);
+computeStack.addDependency(ecrStack);
+computeStack.addDependency(cacheStack);
